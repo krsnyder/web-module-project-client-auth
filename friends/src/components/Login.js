e2c682b27181d6ce7a0304a1dfb0ea5ff15672db
Kirk <kirkrobertsnyder@gmail.com>
2021-03-17 13:27:37 -0400

    Built out login form with username and password inputs and a submit button

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+const emptyCredentials = {
+  username: "",
+  password: ""
+}
 
 export default function Login() {
+  const [credentials, setCredentials] = useState(emptyCredentials)
+
+  const onChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+}
+
+  const login = e => {
+    e.preventDefault()
+  }
+
   return (
     <div className="login-content">
-      Login
+      Login Page
+      <div className="login-form">
+        <form onSubmit={login}>
+          <label>
+            Username:
+             <input
+              type="text"
+              name="username"
+              value={credentials.username}
+              onChange={onChange}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={credentials.password}
+              onChange={onChange}
+            />
+          </label>
+          <button type="submit">Log In</button>
+        </form>
+        
+      </div>
     </div>
   )
 }
